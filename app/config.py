8cc13274652a31ba28f21ebdc891112150d4a645
Delano Senior <dsenior101@hotmail.com>
2021-03-21 19:02:53 -0500

    Config file wasn't saved with new Database URL, final update

--- app/config.py
+++ app/config.py
@@ -5,7 +5,7 @@ class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
     UPLOAD_FOLDER = 'D:/Documents/INFO3180/info3180_project1/uploads'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://dsenior:IlIlIIIl@localhost/project1'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://ojqkpihsfupfiz:b1d819c556e27d576d45a18057809d8406f5da622bb96329f8fb9da45411fbc8@ec2-52-71-161-140.compute-1.amazonaws.com:5432/d58lu1v9hff3p1'
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
 
 class DevelopmentConfig(Config):
